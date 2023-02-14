92bdb1a34a511d59c11c6552618bcb0d68a66e77
Ruben Gonzalez <gonzalezruben@google.com>
2023-02-13 23:09:40 -0600

    fix: include key pair config

--- sample-rest-api-serverless/deploy-rest-api-serverless.sh
+++ sample-rest-api-serverless/deploy-rest-api-serverless.sh
@@ -19,7 +19,22 @@ export PATH=$PATH:$HOME/.apigeecli/bin
 
 TOKEN=$(gcloud auth print-access-token)
 
+echo "Generating signing keys..."
+mkdir certs
+openssl req -subj '/CN=apigee.google.com/O=John Wayne/C=US' -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -keyonly -keyout ./certs/server.key -out ./certs/server.crt
+PR_KEY=$(cat ./certs/server.key)
+echo $PR_KEY
+PU_KEY=$(cat ./certs/server.crt)
+echo $PU_KEY
+PR_KEY=$(printf '%s\n' "$PR_KEY" | tr -d '\n')
+PU_KEY=$(printf '%s\n' "$PU_KEY" | tr -d '\n')
+
 echo "Deploying Apigee artifacts..."
+
+echo "Deploying public and private keys for WS-Security signatures..."
+echo -e "private_key=$PR_KEY\ncertificate=$PU_KEY" > ws_sec_configuration.properties
+apigeecli res create --org "$PROJECT" --env "$APIGEE_ENV" --token "$TOKEN" --name ws_sec_configuration --type properties --respath ws_sec_configuration.properties
+rm ws_sec_configuration.properties
 
 echo "Creating API Product"
 apigeecli products create --name rest-api-serverless-sample-product --displayname "rest-api-serverless-sample-product"  --envs "$APIGEE_ENV" --scopes "READ" --scopes "WRITE" --scopes "ACTION" --approval auto --quota 50 --interval 1 --unit minute --opgrp ./apiproduct-opgroup.json --org "$PROJECT" --token "$TOKEN"
