440d7f7b83c9249d48d34990abfa462da1327de6
Ruben Gonzalez <gonzalezruben@google.com>
2023-02-14 20:07:21 -0600

    fix: basic auth scripts

--- sample-basic-authn/clean-up-basic-authn.sh
+++ sample-basic-authn/clean-up-basic-authn.sh
@@ -25,49 +25,25 @@ if [ -z "$APIGEE_ENV" ]; then
 fi
 
 TOKEN=$(gcloud auth print-access-token)
-APP_NAME=authz-idp-acccess-tokens-sample-app
+APP_NAME=basic-auth-sample-app
 
 echo "Installing apigeecli"
 curl -s https://raw.githubusercontent.com/apigee/apigeecli/master/downloadLatest.sh | bash
 export PATH=$PATH:$HOME/.apigeecli/bin
 
 echo "Deleting Developer App"
-DEVELOPER_ID=$(apigeecli developers get --email authz-idp-acccess-tokens_apigeesamples@acme.com --org "$PROJECT" --token "$TOKEN" --disable-check | jq .'developerId' -r)
+DEVELOPER_ID=$(apigeecli developers get --email basic-auth_apigeesamples@acme.com --org "$PROJECT" --token "$TOKEN" --disable-check | jq .'developerId' -r)
 apigeecli apps delete --id "$DEVELOPER_ID" --name $APP_NAME --org "$PROJECT" --token "$TOKEN"
 
 echo "Deleting Developer"
-apigeecli developers delete --email authz-idp-acccess-tokens_apigeesamples@acme.com --org "$PROJECT" --token "$TOKEN"
+apigeecli developers delete --email basic-auth_apigeesamples@acme.com --org "$PROJECT" --token "$TOKEN"
 
 echo "Deleting API Products"
-apigeecli products delete --name authz-idp-acccess-tokens-sample-product --org "$PROJECT" --token "$TOKEN"
+apigeecli products delete --name basic-auth-sample-product --org "$PROJECT" --token "$TOKEN"
 
-echo "Undeploying sample-authorize-idp-access-tokens proxy"
-REV=$(apigeecli envs deployments get --env "$APIGEE_ENV" --org "$PROJECT" --token "$TOKEN" --disable-check | jq .'deployments[]| select(.apiProxy=="sample-authorize-idp-access-tokens").revision' -r)
-apigeecli apis undeploy --name sample-authorize-idp-access-tokens --env "$APIGEE_ENV" --rev "$REV" --org "$PROJECT" --token "$TOKEN"
+echo "Undeploying sample-basic-auth proxy"
+REV=$(apigeecli envs deployments get --env "$APIGEE_ENV" --org "$PROJECT" --token "$TOKEN" --disable-check | jq .'deployments[]| select(.apiProxy=="sample-basic-auth").revision' -r)
+apigeecli apis undeploy --name sample-basic-auth --env "$APIGEE_ENV" --rev "$REV" --org "$PROJECT" --token "$TOKEN"
 
-echo "Deleting proxy sample-authorize-idp-access-tokens proxy"
-apigeecli apis delete --name sample-authorize-idp-access-tokens --org "$PROJECT" --token "$TOKEN"
-
-echo "Undeploying authorize-idp-access-tokens sharedflow"
-REV=$(apigeecli envs deployments get --env "$APIGEE_ENV" --org "$PROJECT" --token "$TOKEN" --sharedflows true --disable-check | jq .'deployments[]| select(.apiProxy=="authorize-idp-access-tokens").revision' -r)
-apigeecli sharedflows undeploy --name authorize-idp-access-tokens --env "$APIGEE_ENV" --rev "$REV" --org "$PROJECT" --token "$TOKEN"
-
-echo "Deleting proxy authorize-idp-access-tokens sharedflow"
-apigeecli sharedflows delete --name authorize-idp-access-tokens --org "$PROJECT" --token "$TOKEN"
-
-echo "Deleting IdP config environment property set..."
-apigeecli res delete --org "$PROJECT" --env "$APIGEE_ENV" --token "$TOKEN" --name idp_configuration --type properties
-
-rm idp_configuration.properties
-
-echo "Undeploying authorization-server-mock proxy"
-REV=$(apigeecli envs deployments get --env "$APIGEE_ENV" --org "$PROJECT" --token "$TOKEN" --disable-check | jq .'deployments[]| select(.apiProxy=="authorization-server-mock").revision' -r)
-apigeecli apis undeploy --name authorization-server-mock --env "$APIGEE_ENV" --rev "$REV" --org "$PROJECT" --token "$TOKEN"
-
-echo "Deleting proxy authorization-server-mock proxy"
-apigeecli apis delete --name authorization-server-mock --org "$PROJECT" --token "$TOKEN"
-
-echo "Deleting mock config environment property set..."
-apigeecli res delete --org "$PROJECT" --env "$APIGEE_ENV" --token "$TOKEN" --name mock_configuration --type properties
-
-rm mock_configuration.properties
+echo "Deleting proxy sample-basic-auth proxy"
+apigeecli apis delete --name sample-basic-auth --org "$PROJECT" --token "$TOKEN"
